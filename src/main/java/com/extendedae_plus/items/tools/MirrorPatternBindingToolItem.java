4d73354674c0d879b36b96c36dc7cd7d4b0abcea
GaLi <3096147684@qq.com>
2026-04-07 15:39:27 +0800

    绑定工具使用逻辑调整

--- src/main/java/com/extendedae_plus/items/tools/MirrorPatternBindingToolItem.java
+++ src/main/java/com/extendedae_plus/items/tools/MirrorPatternBindingToolItem.java
@@ -33,11 +33,19 @@ public class MirrorPatternBindingToolItem extends Item {
     }
 
     @Override
+    public InteractionResult onItemUseFirst(ItemStack stack, UseOnContext context) {
+        return this.handleBlockUse(context, stack);
+    }
+
+    @Override
     public InteractionResult useOn(UseOnContext context) {
+        return this.handleBlockUse(context, context.getItemInHand());
+    }
+
+    public InteractionResult handleBlockUse(UseOnContext context,ItemStack stack) {
         var level = context.getLevel();
         var player = context.getPlayer();
         var blockEntity = level.getBlockEntity(context.getClickedPos());
-        var stack = context.getItemInHand();
 
         if (blockEntity instanceof PatternProviderBlockEntity
                 && !(blockEntity instanceof MirrorPatternProviderBlockEntity)) {
